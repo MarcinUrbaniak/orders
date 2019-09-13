59bd561c6b65b97d276229ac2751bae675112a7c
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-09-13 08:41:54 +0200

    OrdersApp add constructor and main method - server start

--- src/main/java/com/example/orders/OrdersApp.java
+++ src/main/java/com/example/orders/OrdersApp.java
@@ -1,4 +1,25 @@
 package com.example.orders;
 
-public class OrdersApp {
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+
+public class OrdersApp extends NanoHTTPD {
+
+
+    public OrdersApp(int port) throws IOException {
+        super(port);
+        start(5000, false);
+        System.out.println("Server has been started");
+    }
+
+    public static void main(String[] args) {
+        try {
+            new OrdersApp(8080);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
