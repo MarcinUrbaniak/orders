de0329815d51f66f07e3a36ff329c07b7c5d0e04
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-09-13 08:47:32 +0200

    Book, Customer, Order - add methods

--- src/main/java/com/example/orders/type/Book.java
+++ src/main/java/com/example/orders/type/Book.java
@@ -1,4 +1,11 @@
 package com.example.orders.type;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Book {
+    int book_id;
+    String title;
 }
--- src/main/java/com/example/orders/type/Order.java
+++ src/main/java/com/example/orders/type/Order.java
@@ -1,4 +1,14 @@
 package com.example.orders.type;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class Order {
+    int orderId, customer_id;
+    LocalDate orderDate;
+
 }
