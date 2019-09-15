5d8a701915ccb1f2e727816dd776e956d390419c
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-09-15 16:33:31 +0200

    OrderController - add methods (without body)

--- src/main/java/com/example/orders/controller/OrderController.java
+++ src/main/java/com/example/orders/controller/OrderController.java
@@ -1,4 +1,17 @@
 package com.example.orders.controller;
 
+import com.example.orders.storage.OrderStorage;
+import com.example.orders.storage.impl.OrderStorageImpl;
+import fi.iki.elonen.NanoHTTPD.*;
+
 public class OrderController {
+
+    private OrderStorage orderStorage = new OrderStorageImpl();
+
+    public Response serveGetOrderRequest(IHTTPSession session){return null;}
+
+    public Response serveGetOrdersRequest(IHTTPSession session){return null;}
+
+    public Response serveAddOrderRequest(IHTTPSession session){return null;}
+
 }
