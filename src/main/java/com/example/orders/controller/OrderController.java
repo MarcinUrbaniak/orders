e8b5c1876a878c29bbe6e7166c8154860a9a5aa8
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-09-16 08:24:56 +0200

    OrderController - add serveAddOrderRequest clear code

--- src/main/java/com/example/orders/controller/OrderController.java
+++ src/main/java/com/example/orders/controller/OrderController.java
@@ -47,27 +47,22 @@ public class OrderController {
 
     public Response serveAddOrderRequest(IHTTPSession session){
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
+        List<OrderItem> orderItems = new ArrayList<>();
+        Order order = new Order();
 
         String headers = session.getHeaders().get("content-length");
         int contentLength = Integer.parseInt(headers);
         byte[] buffer = new byte[contentLength];
 
-
-        List<OrderItem> orderItems = new ArrayList<>();
-        Order order = new Order();
-
         try {
             session.getInputStream().read(buffer,0,contentLength);
             String requestBody = new String(buffer);
-
+            //create order
               JsonNode jsonNodeRoot = objectMapper.readTree(requestBody);
               JsonNode jsonOrder = jsonNodeRoot.get(0);
-
               order.setCustomer_id(jsonOrder.get("customer_id").asInt());
               order.setOrderDate(Date.valueOf(jsonOrder.get("orderDate").asText()));
-
+            //create orderItem list
             for (int i = 1; i < jsonNodeRoot.size(); i++) {
                 OrderItem orderItem = new OrderItem();
                 JsonNode jsonOrderItem = jsonNodeRoot.get(i);
@@ -77,9 +72,6 @@ public class OrderController {
 
                 orderItems.add(orderItem);
             }
-
-            System.out.println("jsonNodeRoot = " + jsonNodeRoot.size());
-            //orderItems = Arrays.asList(objectMapper.readValue(requestBody, OrderItem[].class));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -88,35 +80,6 @@ public class OrderController {
              ) {
             System.out.println("orderItem = " + orderItem);
         }
-
         return null;
     }
-
-//    public static void main(String[] args) {
-//        String json=" ["
-//                + "{\"book_id\":21 , \"ammount\":23 , \"order_id\":2}, "
-//                + "{\"book_id\":22 , \"ammount\": 34 , \"order_id\": 5}"
-//                + "]";
-//        System.out.println(json);
-//        try
-//        {
-//            ObjectMapper mapper = new ObjectMapper();
-//            //Convereted to Type as array
-//            OrderItem[] orderItems= mapper.readValue(json,  OrderItem[].class );
-//
-//            for (OrderItem orderItem: orderItems
-//                 ) {
-//                System.out.println("orderItem = " + orderItem);
-//            }
-//        }
-//        catch(IOException ex)
-//        {
-//            ex.printStackTrace();
-//        }
-//        catch(Exception ex)
-//        {
-//            ex.printStackTrace();
-//        }
-//
-//    }
 }
