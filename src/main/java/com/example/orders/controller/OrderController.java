5a37208f40c5efa39860f7b8659d7e5ddcc1ada3
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-09-17 22:35:12 +0200

    OrderController -  add method serveChangeOrderRequest

--- src/main/java/com/example/orders/controller/OrderController.java
+++ src/main/java/com/example/orders/controller/OrderController.java
@@ -7,7 +7,6 @@ import com.example.orders.type.OrderItem;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
